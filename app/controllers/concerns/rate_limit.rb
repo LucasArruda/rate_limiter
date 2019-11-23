91350e58cba1005600bcc75b7045b2b8d64487af
lucasarruda <lucasarruda@gmail.com>
2019-11-23 20:58:58 -0300

    Request counter

--- app/controllers/concerns/rate_limit.rb
+++ app/controllers/concerns/rate_limit.rb
@@ -11,4 +11,15 @@ module RateLimit
     render json: { message: 'Rate limit exceeded. Try again in x seconds' },
            status: :too_many_requests
   end
+
+  def count
+    requester_ip = request.env['REMOTE_ADDR']
+    client_counter_key = "#{requester_ip}_counter"
+
+    counter = $redis.get(client_counter_key)
+
+    $redis.set(client_counter_key, 0) unless counter
+
+    $redis.incr(client_counter_key)
+  end
 end
