39990aa3266f4bce50dea83f8a09d966a7b26fcf
lucasarruda <lucasarruda@gmail.com>
2019-11-23 23:38:31 -0300

    Add expire and show cooldown period in message

--- app/controllers/concerns/rate_limit.rb
+++ app/controllers/concerns/rate_limit.rb
@@ -9,26 +9,44 @@ module RateLimit
     @rate = rate
   end
 
+  def period(period)
+    @period = period
+  end
+
   protected
 
   def defined_rate_limit
     @rate || ENV['RATE_LIMIT'] || 99
   end
 
+  def defined_period
+    @period || ENV['PERIOD'] || 3599
+  end
+
   def rate_limiter
     count
+    increment
 
-    render json: { message: 'Rate limit exceeded. Try again in x seconds' },
-           status: :too_many_requests
+    render_too_many_requests and return if blocked?
+  end
+
+  def render_too_many_requests
+    render json: {
+      message: "Rate limit exceeded. Try again in #{cooldown_period} seconds"
+    }, status: :too_many_requests
   end
 
   def count
-    requester_ip = request.env['REMOTE_ADDR']
-    client_counter_key = "#{requester_ip}_counter"
+    unless counter
+      $redis.set(client_key, 0)
+      $redis.expire(client_key, defined_period)
+    end
+  end
+
+  def increment
     $redis.incr(client_key)
   end
 
-    counter = $redis.get(client_counter_key)
   def blocked?
     counter.present? && counter.to_i > defined_rate_limit.to_i
   end
@@ -37,8 +55,13 @@ module RateLimit
     $redis.get(client_key)
   end
 
+  def cooldown_period
+    $redis.ttl(client_key)
+  end
+
   def client_key
     requester_ip = request.env['REMOTE_ADDR']
-    client_counter_key = "#{requester_ip}_counter"
+
+    "#{requester_ip}_counter"
   end
 end
--- test/controllers/test_controller_test.rb
+++ test/controllers/test_controller_test.rb
@@ -1,19 +1,68 @@
 require 'test_helper'
 
 class TestControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    $redis.flushdb
+  end
+
   test "should get index" do
     get test_index_url
     assert_response :success
   end
 
-  test "should receive 429 after 200 when hits too many requests" do
-    env = ENV.to_hash.merge("RATE_LIMIT" => 2)
+  test "should receive 429 after when already blocked" do
+    env = ENV.to_hash.merge("RATE_LIMIT" => 1)
     Object.stub_const(:ENV, env) do
       get test_index_url
       assert_response :success
 
       get test_index_url
       assert_response :too_many_requests
+    end
+  end
+
+  test "should receive cooldown message with 429 status" do
+    env = ENV.to_hash.merge("RATE_LIMIT" => 1)
+    Object.stub_const(:ENV, env) do
+      get test_index_url
+      assert_response :success
+
+      get test_index_url
+
+      response = JSON.parse(@response.body)
+
+      assert_response :too_many_requests
+      assert response['message'].include?('Rate limit exceeded. Try again in ')
+      assert response['message'].include?(' seconds')
+    end
+  end
+
+  test "should receive 429 after 200 when hits too many requests" do
+    env = ENV.to_hash.merge("RATE_LIMIT" => 2)
+    Object.stub_const(:ENV, env) do
+      2.times {
+        get test_index_url
+        assert_response :success
+      }
+
+      get test_index_url
+      assert_response :too_many_requests
+    end
+  end
+
+  test "should receive 200 after cooldown period" do
+    env = ENV.to_hash.merge("RATE_LIMIT" => 1, "PERIOD" => 60)
+    Object.stub_const(:ENV, env) do
+      get test_index_url
+      assert_response :success
+
+      get test_index_url
+      assert_response :too_many_requests
+
+      travel 61.seconds do
+        get test_index_url
+        assert_response :success
+      end
     end
   end
 end
